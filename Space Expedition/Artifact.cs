19ea150a6291416112e830c7f2f37f166916c4e0
Connor Hall <connrhall204@gmail.com>
2025-07-01 12:39:25 -0500

    Added directory and file reading and initialized Artifact class

---
+++ Space Expedition/Artifact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Expedition {
+    internal class Artifact {
+        public string Name { get; set; }
+        public string Planet { get; set; }
+        public string Date { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+
+        public Artifact(string name, string planet, string date, string location, string description) {
+            Name = name;
+            Planet = planet;
+            Date = date;
+            Location = location;
+            Description = description;
+        }
+
+        public override string ToString () {
+            return $"{Name} | {Planet} | {Date} | {Location} | {Description}";
+        }
+    }
+}
