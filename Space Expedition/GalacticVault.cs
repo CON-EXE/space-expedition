67c77f1904650a83e09b1ccc212f210a1b384756
Connor Hall <connrhall204@gmail.com>
2025-07-01 18:35:29 -0500

    Added Galactic vault class and created startup method

---
+++ Space Expedition/GalacticVault.cs
@@ -0,0 +1,58 @@
+﻿namespace Space_Expedition {
+    internal class GalacticVault {
+        public Artifact[] Inventory { get; set; }
+
+        public GalacticVault () {
+            Inventory = new Artifact[0];
+        }
+
+        public void StartUp (string database) {
+
+            string input = "";
+
+            if (Directory.Exists(database)) {
+                Console.WriteLine("Vault Database found");
+                string[] files = Directory.GetFiles(database);
+                Console.WriteLine("Number of files found: " + files.Length);
+
+                if (files.Length > 0) {
+                    Console.WriteLine("Accessing Artifact Data...");
+
+                    foreach (string file in files) {
+                        ProcessFile(file);
+                    }
+                    Console.WriteLine("Artifact data processed successfully!");
+                    Console.Write("Press any key to continue...");
+                    Console.ReadKey();
+
+                } else {
+                    Console.WriteLine("No artifact data found.");
+                    Console.Write("Press any key to continue...");
+                    Console.ReadKey();
+                }
+
+            } else {
+                Console.Write("Unable to locate Vault Database.");
+                Console.Write("Press any key to continue...");
+                Console.ReadKey();
+            }
+
+            Console.Clear();
+        }
+
+        public void ProcessFile (string file) {
+            try {
+                using (StreamReader reader = new StreamReader(file)) {
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null) {
+                        string[] parameters = line.Split(",");
+
+                    }
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"An exception occured while attempting to access {file}: {ex.Message}");
+            }
+        }
+    }
+}
--- Space Expedition/Program.cs
+++ Space Expedition/Program.cs
@@ -1,35 +1,14 @@
 ﻿using System.Diagnostics;
 using System.Text.RegularExpressions;
-using static System.IO.Directory;
 
 namespace Space_Expedition {
     internal class Program {
         static void Main (string[] args) {
+            GalacticVault vault = new GalacticVault();
 
-            if (Exists("Artifacts")) {
-                Console.WriteLine("Vault Database found");
-            }
-            string[] files = GetFiles("Artifacts");
-            Console.WriteLine("Number of files found: " + files.Length);
-            foreach (string file in files) {
-                ReadFile(file);
-            }
+            string path = "Artifacts"; // File Location
 
-
-        }
-
-        static void ReadFile (string file) {
-            try {
-                using (StreamReader reader = new StreamReader(file)) {
-                    string line = "";
-                    while ((line = reader.ReadLine()) != null) {
-                        Console.WriteLine(line);
-                    }
-                }
-            }
-            catch (Exception ex) {
-                Console.WriteLine($"An exception occured while attempting to access {file}: {ex.Message}");
-            }
+            vault.StartUp(path);
         }
     }
 }
